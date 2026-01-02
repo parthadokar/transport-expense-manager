f4d37a2b412bfbff06dba491c0ef0a1875cbe326
parthadokar <adokar.parth@gmail.com>
2026-01-02 20:02:42 +0530

    frontend&backendupdated

--- expense-manager-frontend/src/App.jsx
+++ expense-manager-frontend/src/App.jsx
@@ -1,21 +1,99 @@
 import { useEffect, useState } from 'react'
 import './index.css'
-import { getTasks } from './services/tasksApi'
+import { getTasks,postTask,updateTask,deleteTask } from './services/tasksApi'
 
 function App() {
   const [tasks,setTasks] = useState([])
+  const [editingId,setEditingId] = useState(null)
+  const [form,setForm] = useState({
+    name: '',
+    total_cost: '',
+    total_spent: ''
+  })
+
   useEffect(() => {
-    async function load() {
-      const data = await getTasks()
-      setTasks(data)
-    }
-    load()
+    loadTasks()
   },[])
 
+  async function loadTasks() {
+    console.log('Fetching tasks...')
+    const data = await getTasks()
+    console.log('Tasks received:', data)
+    setTasks(data)
+  }
+   function handleChange(e) {
+    setForm({
+      ...form,
+      [e.target.name]: e.target.value
+    })
+  }
+  async function handleSubmit(e) {
+    e.preventDefault()
+    const payload = {
+      name: form.name,
+      total_cost: Number(form.total_cost),
+      total_spent: Number(form.total_spent) 
+    }
+
+    if(editingId) {
+      await updateTask(editingId,payload)
+    } else {
+      await postTask(payload)
+    }
+    setForm({name: '',total_cost: '',total_spent: ''})
+    setEditingId(null)
+    loadTasks()
+  }
+
+  function startEdit(task) {
+    setEditingId(task.id)
+    setForm({
+      name: task.name,
+      total_cost: task.total_cost ?? '', 
+      total_spent: task.total_spent ?? ''
+    })
+  }
+
+  async function handleDelete(id) {
+    await deleteTask(id)
+    loadTasks()
+  }
+  
   return (
     <div>
       <h1>Transport Expense Manager</h1>
-      <div>
+      <form onSubmit={handleSubmit}>
+        <input
+          name="name"
+          placeholder="Task name"
+          value={form.name}
+          onChange={handleChange}
+          required
+        />
+
+        <input
+          name="total_cost"
+          type="number"
+          placeholder="Total cost"
+          value={form.total_cost}
+          onChange={handleChange}
+        />
+
+        <input
+          name="total_spent"
+          type="number"
+          placeholder="Total spent"
+          value={form.total_spent}
+          onChange={handleChange}
+        />
+
+        <button type="submit">
+          {editingId ? 'Update Task' : 'Add Task'}
+        </button>
+      </form>
+      <hr />
+
+      <div className='task'>
         {tasks.length === 0 ? (
           <p>No tasks found</p>
         ): (
@@ -25,13 +103,14 @@ function App() {
               <p>Total cost: {task.total_cost}</p>
               <p>Total spent: {task.total_spent}</p>
               <p>Profit: {task.profit}</p>
-              <hr />
+              <button onClick={() => startEdit(task)}>Edit</button>
+              <button onClick={() => handleDelete(task.id)}>Delete</button>
+              <hr/>
             </div>
           ))
         )}
       </div>
     </div>
-    
   )
 }
 
--- expense-manager-frontend/src/services/tasksApi.js
+++ expense-manager-frontend/src/services/tasksApi.js
@@ -1,4 +1,5 @@
 import axios from 'axios'
+
 const BASE_URL = 'http://localhost:8000'
 
 export async function getTasks() {
@@ -11,8 +12,31 @@ export async function getTasks() {
     }
 }
 
-// CREATE
-// GET
-// GET ONE
-// DELETE
-// UPDATE
+export async function postTask(taskData) {
+    try {
+        const response = await axios.post(`${BASE_URL}/task`,taskData)
+        return response.data
+    } catch (error) {
+        console.error('Error Pushing Task',error)
+        throw error
+    }
+}
+
+export async function updateTask(id,taskData) {
+    try {
+        const response = await axios.patch(`${BASE_URL}/task/${id}`)
+        return response.data
+    } catch (error) {
+        console.error('Error Updating Task',error)
+        throw error
+    }
+}
+
+export async function deleteTask(id) {
+    try {
+        await axios.delete(`${BASE_URL}/tasks/{id}`)
+    } catch (error) {
+        console.error('Error Deleting Task',error)
+        throw error
+    }
+}
