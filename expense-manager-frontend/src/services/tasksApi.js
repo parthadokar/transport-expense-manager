74f85f535c25832448081aafa8d1556bc90288f3
parthadokar <adokar.parth@gmail.com>
2026-01-05 17:04:58 +0530

    deploying frontend

--- expense-manager-frontend/src/services/tasksApi.js
+++ expense-manager-frontend/src/services/tasksApi.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const BASE_URL = 'http://localhost:8000'
+const BASE_URL = import.meta.env.VITE_API_BASE_URL
 
 export async function getTasks() {
   return (await axios.get(`${BASE_URL}/tasks`)).data
